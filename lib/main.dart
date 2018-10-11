3966fef56138d63853d42159460de491cf2294b6
MrAnsheff <mransheff86@gmail.com>
2018-10-11 12:48:06 +0400

    Persistent Footer Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  String _value = '';
+
+  void _onClick(String value) => setState((){_value = value;});
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name Here'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-          padding: EdgeInsets.all(32.0),
-          child: new Column(
-            children: <Widget>[
-              new Text('This is the Drawer'),
-              new RaisedButton(onPressed: () => Navigator.pop(context), child: new Text('Close Drawer'),),
-            ],
-          ),
-        ),
-      ),
+      persistentFooterButtons: <Widget>[
+        new IconButton(icon: new Icon(Icons.add), onPressed: () => _onClick('value1'),),
+        new IconButton(icon: new Icon(Icons.laptop), onPressed: () => _onClick('value2'),),
+        new IconButton(icon: new Icon(Icons.kitchen), onPressed: () => _onClick('value3'),),
+      ],
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('All Widgets Here'),
+              new Text(_value),
             ],
           ),
         ),
