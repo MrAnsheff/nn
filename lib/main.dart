b7183c1fb9ef5a8a1381693c6d490d175f8bd5f7
MrAnsheff <mransheff86@gmail.com>
2018-10-14 19:55:48 +0400

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-
-  String _value = "";
-  GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
-
-  void _setName(String value) => setState((){_value = value;});
-
-  void _openSnack(){
-    _scaffoldState.currentState.showSnackBar(new SnackBar(content: new Text(_value),));
-
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      key: _scaffoldState,
       appBar: new AppBar(
         title: new Text('Name Here'),
       ),
@@ -35,8 +23,42 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new TextField(onChanged: _setName),
-              new RaisedButton(child: new Text("Send to SnackBar"), onPressed: _openSnack,),
+
+              new Card(
+                child: new Container(
+                  padding: EdgeInsets.all(32.0),
+                  child: new Column(
+                    children: <Widget>[
+                    new Text("First Line"),
+                    new Text("Second Line"),
+                    ],
+                  ),
+                ),
+              ),
+              new Card(
+                child: new Container(
+                  padding: EdgeInsets.all(32.0),
+                  child: new Column(
+                    children: <Widget>[
+                    new Text("First Line"),
+                    new Text("Second Line"),
+                    ],
+                  ),
+                ),
+              ),
+              new Card(
+                child: new Container(
+                  padding: EdgeInsets.all(32.0),
+                  child: new Column(
+                    children: <Widget>[
+                    new Text("First Line"),
+                    new Text("Second Line"),
+                    ],
+                  ),
+                ),
+              ),
+
+
             ],
           ),
         ),
