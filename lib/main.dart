aca117b3f564a875653fc315296adc441139c0fb
MrAnsheff <mransheff86@gmail.com>
2018-10-15 09:54:24 +0400

    ToolTip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(new MaterialApp(
@@ -15,25 +12,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  Map _countries = new Map();
+  String _value = "No Data";
 
-
-
-  void _getCountries() async {
-    var url = "http://country.io/names.json";
-    var response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      setState(() {
-        _countries = json.decode(response.body);
-        print(_countries.length);
-      });
-    }
+  void _onChange(){
+    setState(() {
+          _value = new DateTime.now().toString();
+        });
   }
-
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name Here'),
@@ -41,28 +28,16 @@ class _State extends State<MyApp> {
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
-          child: new ListView.builder(
-            itemCount: _countries.length,
-            itemBuilder: (BuildContext context, int index){
-              String key = _countries.keys.elementAt(index);
-              return Card(
-                child: new Container(
-                  padding: EdgeInsets.all(32.0),
-                  child: new Text('$key : ${_countries[key]}'),
-                ),
-              );
-            },
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new IconButton(icon: new Icon(Icons.time_to_leave,),
+              onPressed: _onChange,
+              tooltip: "Click Me!",)
+            ],
           ),
         ),
       ),
     );
   }
-
-   
- @override 
- void initState() {
-     // TODO: implement initState
-     _getCountries();
-   }
-   
 }
