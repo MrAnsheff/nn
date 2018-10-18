bcaa103fa0b5e91ee02bf5db265c804898658038
MrAnsheff <mransheff86@gmail.com>
2018-10-18 16:06:46 +0400

    StatelessWidget Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'mywidget.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -13,41 +12,7 @@ class MyApp extends StatefulWidget {
   State createState() => new _State();
 }
 
-class Sales{
-  int year;
-  int sales;
-  charts.Color color;
-  Sales(this.year, this.sales, this.color);
-}
-
 class _State extends State<MyApp> {
-  List<Sales> _data;
-  List<charts.Series<Sales, int>> _chartData;
-
-  @override
-    void initState() {
-      // TODO: implement initState
-      _makeData();
-    }
-
-  _makeData(){
-    _chartData = new List<charts.Series<Sales, int>>();
-    _data = <Sales>[
-      new Sales(0, 100, charts.MaterialPalette.green.shadeDefault),
-      new Sales(1, 89, charts.MaterialPalette.red.shadeDefault),
-      new Sales(2, 50, charts.MaterialPalette.blue.shadeDefault),
-      new Sales(3, 16, charts.MaterialPalette.teal.shadeDefault),
-      new Sales(4, 4, charts.MaterialPalette.yellow.shadeDefault),
-    ];
-
-    _chartData.add(new charts.Series(
-      id: "sales",
-      data: _data,
-      domainFn: (Sales sale,__) => sale.year,
-      measureFn: (Sales sale,__) => sale.sales,
-      colorFn: (Sales sale,__) => sale.color,
-    ));
-  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -59,8 +24,8 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Pie Chart'),
-              new Expanded(child: new charts.PieChart(_chartData, animate: true, animationDuration: new Duration(seconds: 6),)),
+              new Text('All Widgets Here'),
+              new MyWidget(),
             ],
           ),
         ),
---
+++ lib/mywidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget{
+  build(context){
+    return new Container(
+      padding: new EdgeInsets.all(32.0),
+      child: new Text('My new widget'),
+    );
+  }
+}
