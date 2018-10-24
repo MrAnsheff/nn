cf571514e73b3ad23f48599dca4913957e62b9f7
MrAnsheff <mransheff86@gmail.com>
2018-10-24 11:43:40 +0400

    Redux plus Flutter Redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
-import './screens/home.dart';
-import './screens/second.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-void main(){
-  runApp(new MyApp());
+enum Action { Increment, Decrement }
+
+int reducer(int state, dynamic action) {
+  if (action == Action.Increment) state++;
+  if (action == Action.Decrement) state--;
+  return state;
 }
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Navigation',
-      routes: <String, WidgetBuilder>{
-        '/Home': (BuildContext context) => new Home(),
+void main() {
+  final store = new Store<int>(reducer, initialState: 0);
+  runApp(
+    new MyApp(store: store),
+  );
+}
 
-      },
-      home: new Home(),
+class MyApp extends StatelessWidget {
+  MyApp({Key key, this.store});
+  final Store<int> store;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return StoreProvider(
+      store: store,
+      child: new MaterialApp(
+        title: "Reudux Testing",
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: Text('Flutter Redux'),
+          ),
+          body: new Container(
+            padding: EdgeInsets.all(32.0),
+            child: new Center(
+              child: new Column(
+                children: <Widget>[
+
+                  new StoreConnector<int, String>(
+                    builder: (BuildContext context, count) => new Text(
+                          count,
+                          style: new TextStyle(fontSize: 32.0),
+                        ),
+                    converter: (store) => store.state.toString(),
+                  ),
+
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new IconButton(icon: Icon(Icons.add), onPressed: () => store.dispatch(Action.Increment),),
+                      new IconButton(icon: Icon(Icons.remove), onPressed: () => store.dispatch(Action.Decrement),),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
