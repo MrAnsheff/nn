5020d9969ebb189d72473203ae422bfeee3128aa
MrAnsheff <mransheff86@gmail.com>
2018-10-30 18:42:43 +0300

    AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -12,44 +12,56 @@ class MyApp extends StatefulWidget {
   State createState() => new _State();
 }
 
-class _State extends State<MyApp> with SingleTickerProviderStateMixin{
-  Animation<double> animation;
+class AnimatedLogo extends AnimatedWidget{
+
+  final _opacityTween = new Tween(begin: 0.1, end: 1.0);
+  final _rotateTween = new Tween(begin: 0.0, end: 12.0);
+  final _sizeTween = new Tween(begin: 0.0, end: 300.0);
+
+  AnimatedLogo({Key key, Animation<double> animation}) : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context){
+    final Animation<double> animation = listenable;
+    return new Transform.rotate(angle: _rotateTween.evaluate(animation), 
+    child: new Opacity(opacity: _opacityTween.evaluate(animation),
+    child: new Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      height: _sizeTween.evaluate(animation),
+      width: _sizeTween.evaluate(animation),
+      child: new Image.asset('images/lan.png'),
+    ),
+    )
+    );
+
+  }
+}
+
+class _State extends State<MyApp> with TickerProviderStateMixin{
+  Animation animation;
   AnimationController controller;
 
   @override
   void initState(){
     super.initState();
-    controller = new AnimationController(duration: const Duration(milliseconds: 1000),vsync: this);
-    final CurvedAnimation curve = new CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
-    animation = new Tween(begin: 200.0, end: 300.0).animate(curve);
+    controller = new AnimationController(duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
     animation.addStatusListener(listener);
 
     controller.forward();
   }
 
+  void listener(AnimationStatus status){
+    if(status == AnimationStatus.completed){
+      controller.reverse();
+    } else if (status == AnimationStatus.dismissed){
+      controller.forward();
+    }
+  }
   @override
   void dispose(){
     controller.dispose();
     super.dispose();
   }
-
-  void listener(AnimationStatus status){
-    if(status == AnimationStatus.completed){
-      controller.reverse();
-    } else if(status == AnimationStatus.dismissed){
-      controller.forward();
-    }
-
-  }
-
-  Widget builder(BuildContext context, Widget child){
-    return new Container(
-      height: animation.value,
-      width: animation.value,
-      child: new Image.asset('images/lan.png')
-    );
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +72,7 @@ class _State extends State<MyApp> with SingleTickerProviderStateMixin{
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
-          child: new AnimatedBuilder(animation: animation, builder: builder,),
+          child: new AnimatedLogo(animation: animation),
         ),
       ),
     );
